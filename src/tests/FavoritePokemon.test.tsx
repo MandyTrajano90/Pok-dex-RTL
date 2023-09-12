5b61f1d41a6f5b2d4f2a3816fca81b913659f287
Mandytrajano90 <mandytrajano90@gmail.com>
2023-09-12 16:39:45 -0300

    Tests of Favorite Pokemon

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,17 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <FavoritePokemon.tsx />', () => {
+  test('É exibida na tela a mensagem No favorite pokemon found caso a pessoa não tenha Pokémon favorito', () => {
+    renderWithRouter(<App />, { route: '/favorites' });
+    expect(screen.getByText('No favorite Pokémon found')).toBeInTheDocument();
+  });
+  test('Apenas são exibidos os Pokémon favoritados', async () => {
+    const { user } = renderWithRouter(<App />);
+    await user.click(screen.getByRole('link', { name: /more details/i }));
+    await user.click(screen.getByText(/pokémon favoritado\?/i));
+    await user.click(screen.getByRole('link', { name: /favorite pokémon/i }));
+    expect(screen.getByTestId(/pokemon-name/i)).toBeInTheDocument();
+  });
+});
