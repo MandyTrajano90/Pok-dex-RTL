e409155592aa91181f5d48ab35d9d46ddf49f022
Mandytrajano90 <mandytrajano90@gmail.com>
2023-09-12 14:00:51 -0300

    Initial commmit

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App ">
       <div className="header-container">
         <header>
           <h1>Pokédex</h1>
