01097ffd127ac57afd3ec05a610576fb781fda6a
Mandytrajano90 <mandytrajano90@gmail.com>
2023-09-12 16:52:55 -0300

    Tests of Not Found pages

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,14 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <NotFound.tsx />', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter((<App />), { route: '*' });
+    expect(screen.getByRole('heading', { name: /Page requested not found/i })).toBeInTheDocument();
+    const textIMG = "Clefairy pushing buttons randomly with text I have no idea what i'm doing";
+    const srcIMG = '/404.gif';
+    expect(screen.getByRole('img')).toHaveAttribute('src', srcIMG);
+    expect(screen.getByRole('img')).toHaveAttribute('alt', textIMG);
+  });
+});
