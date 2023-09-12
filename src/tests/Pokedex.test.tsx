a4e052b70502bd97f36e20f318bb6dbf7461cd6f
Mandytrajano90 <mandytrajano90@gmail.com>
2023-09-12 20:04:38 -0300

    Tests of Pokemon Cards

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -1,1 +1,43 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+const dataID = 'pokemon-name';
+const nextPokemon = /próximo pokémon/i;
+
+describe('Teste o componente <Pokedex.tsx />', () => {
+  test('Teste se a página contém um heading h2 com o texto Encountered Pokémon.', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByRole('heading', { name: /Encountered Pokémon/i, level: 2 })).toBeInTheDocument();
+  });
+
+  test('Teste se é exibido o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado', () => {
+    expect(screen.getByRole('button', { name: /próximo pokémon/i })).toBeInTheDocument();
+  });
+
+  test('Os próximos Pokémon da lista devem ser mostrados, um a um, ao clicar sucessivamente no botão', async () => {
+    const { user } = renderWithRouter(<App />);
+    expect(screen.getByTestId(dataID)).toHaveTextContent(/pikachu/i);
+    await user.click(screen.getByText(nextPokemon));
+    expect(screen.getByTestId(dataID)).toHaveTextContent(/charmander/i);
+  });
+
+  test('O primeiro Pokémon da lista deve ser mostrado ao clicar no botão se estiver no último Pokémon da lista', async () => {
+    const { user } = renderWithRouter(<App />);
+    await user.tripleClick(screen.getByText(nextPokemon));
+    await user.tripleClick(screen.getByText(nextPokemon));
+    await user.tripleClick(screen.getByText(nextPokemon));
+    expect(screen.getByTestId(dataID)).toHaveTextContent(/pikachu/i);
+  });
+
+  test('Teste se a Pokédex tem os botões de filtro:', async () => {
+    const { user } = renderWithRouter(<App />);
+    const filtersBtns = screen.getAllByTestId('pokemon-type-button');
+    expect(filtersBtns).toHaveLength(7);
+    expect(screen.getByRole('button', { name: /próximo pokémon/i })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /All/i })).toBeInTheDocument();
+    const psyBtn = filtersBtns[4];
+    await user.click(psyBtn);
+    expect(screen.getByText('Alakazam')).toBeInTheDocument();
+  });
+});
--- src/tests/Pokemon.test.tsx
+++ src/tests/Pokemon.test.tsx
@@ -1,1 +1,36 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <Pokemon.tsx />', () => {
+  test('Teste se é renderizado um card com as informações de determinado Pokémon', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByTestId('pokemon-name')).toHaveTextContent('Pikachu');
+    expect(screen.getByTestId('pokemon-type')).toHaveTextContent('Electric');
+    expect(screen.getByTestId('pokemon-weight')).toHaveTextContent('Average weight: 6.0 kg');
+
+    const imgText = 'Pikachu sprite';
+    const imgSRC = 'https://archives.bulbagarden.net/media/upload/b/b2/Spr_5b_025_m.png';
+    expect(screen.getByRole('img')).toHaveAttribute('alt', imgText);
+    expect(screen.getByRole('img')).toHaveAttribute('src', imgSRC);
+  });
+  test('Teste se o card do Pokémon indicado na Pokédex contém um link de navegação para exibir detalhes desse Pokémon', async () => {
+    const { user } = renderWithRouter(<App />);
+    const detailsLink = screen.getByRole('link', { name: /More details/i });
+    expect(detailsLink).toBeInTheDocument();
+    expect(detailsLink).toHaveAttribute('href', '/pokemon/25');
+    await user.click(detailsLink);
+    expect(screen.getByText(/Summary/i)).toBeInTheDocument();
+  });
+  test('Teste se existe um ícone de estrela nos Pokémon favoritados', async () => {
+    const { user } = renderWithRouter(<App />);
+    const detailsLink = screen.getByRole('link', { name: /More details/i });
+    await user.click(detailsLink);
+    expect(window.location.pathname).toBe('/pokemon/25');
+    expect(screen.getByText(/Summary/i)).toBeInTheDocument();
+    const favPoke = screen.getByRole('checkbox', { name: /Pokémon favoritado?/i });
+    await user.click(favPoke);
+    const star = screen.getByAltText('Pikachu is marked as favorite');
+    expect(star).toHaveAttribute('src', '/star-icon.png');
+  });
+});
