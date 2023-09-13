4754aa93d948ddc3db533d01ef1dd16850d90560
Mandytrajano90 <mandytrajano90@gmail.com>
2023-09-13 10:18:02 -0300

    Tests of Pokedex

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -11,8 +11,30 @@ describe('Teste o componente <Pokedex.tsx />', () => {
     expect(screen.getByRole('heading', { name: /Encountered Pokémon/i, level: 2 })).toBeInTheDocument();
   });
 
-  test('Teste se é exibido o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado', () => {
-    expect(screen.getByRole('button', { name: /próximo pokémon/i })).toBeInTheDocument();
+  test('Teste se é exibido o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado', async () => {
+    const { user } = renderWithRouter(<App />);
+    const nextPoke = screen.getByRole('button', { name: /próximo pokémon/i });
+    expect(nextPoke).toBeInTheDocument();
+    const pokeName = screen.getByTestId(/pokemon-name/i);
+    expect(pokeName).toHaveTextContent(/Pikachu/);
+    await user.click(nextPoke);
+    expect(pokeName).toHaveTextContent(/Charmander/i);
+    await user.click(nextPoke);
+    expect(pokeName).toHaveTextContent(/Caterpie/i);
+    await user.click(nextPoke);
+    expect(pokeName).toHaveTextContent(/Ekans/i);
+    await user.click(nextPoke);
+    expect(pokeName).toHaveTextContent(/Alakazam/i);
+    await user.click(nextPoke);
+    expect(pokeName).toHaveTextContent(/Mew/i);
+    await user.click(nextPoke);
+    expect(pokeName).toHaveTextContent(/Rapidash/i);
+    await user.click(nextPoke);
+    expect(pokeName).toHaveTextContent(/Snorlax/i);
+    await user.click(nextPoke);
+    expect(pokeName).toHaveTextContent(/Dragonair/i);
+    await user.click(nextPoke);
+    expect(pokeName).toHaveTextContent(/Pikachu/i);
   });
 
   test('Os próximos Pokémon da lista devem ser mostrados, um a um, ao clicar sucessivamente no botão', async () => {
@@ -30,14 +52,38 @@ describe('Teste o componente <Pokedex.tsx />', () => {
     expect(screen.getByTestId(dataID)).toHaveTextContent(/pikachu/i);
   });
 
-  test('Teste se a Pokédex tem os botões de filtro:', async () => {
-    const { user } = renderWithRouter(<App />);
+  test('Teste se a Pokédex tem os botões de filtro:', () => {
+    renderWithRouter(<App />);
     const filtersBtns = screen.getAllByTestId('pokemon-type-button');
-    expect(filtersBtns).toHaveLength(7);
-    expect(screen.getByRole('button', { name: /próximo pokémon/i })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: /All/i })).toBeInTheDocument();
-    const psyBtn = filtersBtns[4];
+    const typesOfPoke = ['Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon'];
+    const pokeNames = filtersBtns.map((type) => type.textContent);
+    expect(pokeNames).toEqual(typesOfPoke);
+  });
+
+  test('Após a seleção de um botão de tipo, a Pokédex deve circular somente pelos Pokémon daquele tipo e O botão All precisa estar sempre visível', async () => {
+    const { user } = renderWithRouter(<App />);
+    const psyBtn = screen.getByRole('button', { name: /psychic/i });
     await user.click(psyBtn);
-    expect(screen.getByText('Alakazam')).toBeInTheDocument();
+    expect(psyBtn).toBeInTheDocument();
+    const pokeName = screen.getByTestId(/pokemon-name/i);
+    const nextPoke = screen.getByRole('button', { name: /próximo pokémon/i });
+    await user.click(nextPoke);
+    expect(pokeName).toHaveTextContent(/Mew/i);
+    await user.click(nextPoke);
+    expect(pokeName).toHaveTextContent(/Alakazam/i);
+    expect(screen.getByRole('button', { name: /all/i })).toBeInTheDocument();
+  });
+  test('Teste se a Pokédex contém um botão para resetar o filtro', async () => {
+    const { user } = renderWithRouter(<App />);
+    const allBtn = screen.getByRole('button', { name: /all/i });
+    await user.click(allBtn);
+    expect(allBtn).toBeInTheDocument();
+    const pokeFire = screen.getByRole('button', { name: /fire/i });
+    await user.click(pokeFire);
+    expect(pokeFire).toBeInTheDocument();
+    const pokeName = screen.getByTestId(/pokemon-name/i);
+    expect(pokeName).toHaveTextContent(/Charmander/i);
+    await user.click(allBtn);
+    expect(pokeName).toHaveTextContent(/Pikachu/i);
   });
 });
